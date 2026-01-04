dc0fff236e4f70ff7527a506fabd44dc9e2cd539
HassainHerhmh <aabcaade6@gmail.com>
2026-01-04 16:45:58 +0300

    Update Types.tsx

--- src/pages/Types.tsx
+++ src/pages/Types.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Plus, Trash2, Edit } from "lucide-react";
-import { API_URL } from "../config";
+import { appRoutes } from "../config/routes";
+
 
 interface TypeItem {
   id: number;
