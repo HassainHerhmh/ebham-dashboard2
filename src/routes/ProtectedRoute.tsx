df0c26388e43073a5e9ad060c1ea9e5e06a5ae24
HassainHerhmh <aabcaade6@gmail.com>
2026-01-04 20:12:06 +0300

    Update ProtectedRoute.tsx

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,32 +1,11 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
-import { hasPermission } from "../utils/permissions";
 
-interface Props {
-  children: React.ReactNode;
-  section: string;
-  action?: "view" | "add" | "edit" | "delete" | "print";
-}
+export default function ProtectedRoute({ children }: any) {
+  const user = localStorage.getItem("user");
 
-const ProtectedRoute: React.FC<Props> = ({
-  children,
-  section,
-  action = "view",
-}) => {
-  const user = JSON.parse(localStorage.getItem("user") || "null");
-
-  // غير مسجل دخول
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // غير مصرح
-  if (!hasPermission(user, section, action)) {
-    return <Navigate to="/unauthorized" replace />;
-  }
-
-  // مسموح
-  return <>{children}</>;
-};
-
-export default ProtectedRoute;
+  return children;
+}
