8ffc56b8ee628058c7e4a0e6689d2194b8f0b6ff
HassainHerhmh <aabcaade6@gmail.com>
2026-01-05 12:16:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from "react"; 
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AppProvider } from "./contextS/AppContext";
 import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode> 
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter> 
-  </React.StrictMode> );
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
