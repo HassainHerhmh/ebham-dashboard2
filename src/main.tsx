d4aad555677bfde8894a5b7c241b88e875feb34f
HassainHerhmh <aabcaade6@gmail.com>
2026-01-05 12:18:44 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AppProvider } from "./contextS/AppContext";
+import { AppProvider } from "./contexts/AppContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
