9472100eb34104ce6d5347fff8c90135d5d5bb73
HassainHerhmh <aabcaade6@gmail.com>
2026-01-12 12:48:03 +0300

    Update Restaurants.tsx

--- src/pages/Restaurants.tsx
+++ src/pages/Restaurants.tsx
@@ -30,7 +30,7 @@ interface ScheduleItem {
   closed: boolean;
 }
 
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 const daysOfWeek = [
   "السبت",
   "الأحد",
