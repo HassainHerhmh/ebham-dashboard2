048269d4e2dca3d6d21f63cee5d69928be452a1f
HassainHerhmh <aabcaade6@gmail.com>
2026-01-15 18:17:38 +0300

    Update index.tsx

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -3,6 +3,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const Reports = () => {
   return (
     <div className="space-y-4">
+      <Navigate to="account-statement" replace />
       <Outlet />
     </div>
   );
