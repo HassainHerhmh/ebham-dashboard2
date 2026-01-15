1d3ddf72e6f3419cd747a7b9809672ef5b0f35b5
HassainHerhmh <aabcaade6@gmail.com>
2026-01-15 18:27:35 +0300

    Create AccountStatement.tsx

---
+++ src/pages/Reports/AccountStatement.tsx
@@ -0,0 +1,10 @@
+const AccountStatement = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">كشف حساب</h2>
+      {/* هنا لاحقًا نضع واجهة كشف الحساب */}
+    </div>
+  );
+};
+
+export default AccountStatement;
