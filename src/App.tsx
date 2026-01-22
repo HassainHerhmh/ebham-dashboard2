6613d6847c337f36a664a73dd38b260ee17f1b7e
HassainHerhmh <aabcaade6@gmail.com>
2026-01-22 11:46:03 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ const App: React.FC = () => {
                     <Route path="/users" element={<ProtectedRoute section="users"><Users /></ProtectedRoute>} />             
                    <Route path="/payments/electronic"element={<ProtectedRoute section="payments"><PaymentsElectronic /></ProtectedRoute>}/>
                    <Route path="/payments/banks"element={<ProtectedRoute section="payments"><PaymentsBanks /></ProtectedRoute>}/>
-                     <Route path="/payments/payments-cash" element={ <ProtectedRoute section="settings"><PaymentsCash/></ProtectedRoute>}/>
+                     <Route path="/payments/payments-cash" element={ <ProtectedRoute section="payments"><PaymentsCash/></ProtectedRoute>}/>
 
 
 
