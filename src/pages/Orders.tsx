90df0b70854fe0b5a2902cd295ff0b6f7be98dc4
HassainHerhmh <aabcaade6@gmail.com>
2026-01-24 21:01:52 +0300

    Update Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -51,8 +51,9 @@ interface OrderDetails {
 /* =====================
    Component
 ===================== */
-const socket = io("http://localhost:8080"); // أو دومين الباك إند
- // عدّل حسب عنوان السيرفر
+// في Orders.tsx
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:8080";
+const socket = io(SOCKET_URL);
 
 function ToastNotifications() {
   const [toasts, setToasts] = useState<any[]>([]);
