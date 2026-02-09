f856aec56b056a434dc42a889094838eb11864cb
HassainHerhmh <aabcaade6@gmail.com>
2026-02-09 03:22:56 +0300

    Update ManualOrders.tsx

--- src/pages/ManualOrders.tsx
+++ src/pages/ManualOrders.tsx
@@ -47,10 +47,11 @@ const ManualOrders: React.FC = () => {
     try {
       setLoading(true);
       const [ordersRes, custRes, accRes] = await Promise.all([
-        console.log("ACCOUNTS RAW DATA:", accRes.data);
-console.log("ACCOUNTS LIST:", accRes.data?.list);
+
 
         api.get("/wassel-orders/manual/manual-list"),
+                console.log("ACCOUNTS RAW DATA:", accRes.data);
+console.log("ACCOUNTS LIST:", accRes.data?.list);
         api.get("/customers"),
         api.get("/accounts")
       ]);
